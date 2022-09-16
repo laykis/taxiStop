167f6b0a4ff5d70be07fffb299e26ae14ab90237
laykis <flash6471@naver.com>
2022-09-17 00:17:52 +0900

    first commit

---
+++ src/main/java/WKU/taxiStop/TaxiStopApplication.java
@@ -0,0 +1,13 @@
+package WKU.taxiStop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiStopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WKU/taxiStop/TaxiStopApplicationTests.java
@@ -0,0 +1,13 @@
+package WKU.taxiStop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiStopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
